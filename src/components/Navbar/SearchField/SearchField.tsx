6c29e9328348bdf7afcef6133d969c0d59bcdbef
cr9169 <bar.gabay.home@gmail.com>
2025-01-07 00:29:51 +0200

    feat: implement SearchField component with search icon and styled input, fixed search.svg

--- src/components/Navbar/SearchField/SearchField.tsx
+++ src/components/Navbar/SearchField/SearchField.tsx
@@ -1,9 +1,13 @@
 import "./SearchField.css";
+import searchIcom from "../../../assets/search.svg";
 
-interface SearchFieldProps {}
-
-const SearchField: React.FC<SearchFieldProps> = ({}) => {
-  return <div></div>;
+const SearchField: React.FC = () => {
+  return (
+    <div className="search-field-main-section">
+      <img src={searchIcom} alt="Search icon" />
+      <input type="text" placeholder="Search" />
+    </div>
+  );
 };
 
 export default SearchField;
