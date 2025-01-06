5f53ab27f3867834be4208aa98ebc280853c8648
cr9169 <bar.gabay.home@gmail.com>
2025-01-07 00:23:56 +0200

    feat: implement Navbar component with navigation items and active state indicatorת fixed home.svg, switched logo png

--- src/components/Navbar/NavComponent/NavComponent.tsx
+++ src/components/Navbar/NavComponent/NavComponent.tsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import "./NavComponent.css";
+import homeIcon from "../../../assets/home.svg";
+import messagingIcon from "../../../assets/messaging.svg";
+import notificationsIcon from "../../../assets/notifications.svg";
 
-interface NavComponentProps {}
+type Section = "Home" | "Messaging" | "Notifications";
 
-const NavComponent: React.FC<NavComponentProps> = ({}) => {
-  return <div></div>;
+const navMapping: { [key: string]: string } = {
+  Home: homeIcon,
+  Messaging: messagingIcon,
+  Notifications: notificationsIcon,
+};
+
+const NavComponent: React.FC = () => {
+  const [chosenSection, setChosenSection] = useState<Section>("Home");
+
+  return (
+    <div className="nav-component-main">
+      {Object.entries(navMapping).map(([key, value]) => (
+        <div
+          key={key}
+          className={`nav-item ${chosenSection === key ? "active" : ""}`}
+          onClick={() => setChosenSection(key as Section)}
+        >
+          <img
+            src={value}
+            alt={`${key} icon`}
+            className={`nav-icon ${chosenSection === key ? "active" : ""}`}
+          />
+          <span>{key}</span>
+          {chosenSection === key && <div className="active-indicator" />}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default NavComponent;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,18 @@
 import "./Navbar.css";
+import logo from "../../assets/Tedooo-logo.png";
+import SearchField from "./SearchField/SearchField";
+import NavComponent from "./NavComponent/NavComponent";
 
-interface NavbarProps {}
-
-const Navbar: React.FC<NavbarProps> = ({}) => {
-  return <div></div>;
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar-main-section">
+      <div>
+        <img src={logo} alt="Tedooo logo" />
+        <SearchField />
+      </div>
+      <NavComponent />
+    </div>
+  );
 };
 
 export default Navbar;
